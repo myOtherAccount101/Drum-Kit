6788dc4b374adecebc5059eef3447431cd1c52ff
myOtherAccount101 <128358795+myOtherAccount101@users.noreply.github.com>
2023-10-03 18:45:46 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+document.addEventListener("keydown",function(event){
+    playDrum(event.key);
+    addAnimation(event.key);
+})
+
+function playDrum(key){
+    addAnimation(key);
+    switch (key) {
+        case "w":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            console.log(this.innerHTML);
+            break;
+    }  
+}
+
+var numberOfDrums=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrums;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    playDrum(this.innerHTML);
+});
+}
+
+function addAnimation(key){
+    var activeButton=document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100);
+}
+
